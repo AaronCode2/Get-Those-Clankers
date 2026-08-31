7dca11b6e50f626b71973f7346ab1ae9638ea1df
AaronCode2 <AaronWorkPC@outlook.com>
2026-08-31 22:35:05 +0100

    got inventory started, saved in case something goes wrong

---
+++ src/inventory.py
@@ -0,0 +1,15 @@
+import pygame
+import utils
+
+class Inventory():
+
+    def __init__(self):
+
+        self.toggle = False
+
+    def update(self, window):
+
+        self.draw(window)
+
+    def draw(self, window):
+        pass
