b5fd6e99b851e216e6fd1a830612771ab295bf2a
Jujulien45 <julienmondy10@gmail.com>
2026-09-02 21:42:14 +0200

    naming acceleration becomes private

--- src/classes/objects/entity.py
+++ src/classes/objects/entity.py
@@ -13,16 +13,16 @@ class Entity:
         self.hitbox = pygame.FRect((0, 0), hitbox_size)
         self.hitbox.midbottom = midbottom
         self.velocity = pygame.Vector2(0.0, 0.0)
-        self.acceleration = pygame.Vector2(0.0, 0.0)
+        self._acceleration = pygame.Vector2(0.0, 0.0)
 
     def apply_force(self, force: pygame.Vector2):
-        self.acceleration += force
+        self._acceleration += force
 
     def update(self):
         # This looks weird I know, but it's acctualy the right way to do it
-        self.velocity += self.acceleration * (utils.deltaTime * 0.5)
+        self.velocity += self._acceleration * (utils.deltaTime * 0.5)
         self.hitbox.midbottom += self.velocity * utils.deltaTime
-        self.velocity += self.acceleration * utils.deltaTime * 0.5
+        self.velocity += self._acceleration * utils.deltaTime * 0.5
 
-        self.acceleration.x = 0
-        self.acceleration.y = 0
+        self._acceleration.x = 0
+        self._acceleration.y = 0
