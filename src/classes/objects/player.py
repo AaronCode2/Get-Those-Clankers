baa0efecf3e83c602220b294acdfd9f15f8cb8bd
gurujezz <alexgeddo08@gmail.com>
2026-09-02 22:54:00 +0200

    added a while true

--- src/classes/objects/player.py
+++ src/classes/objects/player.py
@@ -6,8 +6,10 @@ import classes.utility.utils as utils
 class Player(AnimatedEntity):
     def __init__(self, position: pygame.Vector2):
         animation = AnimationManager("player/player.png", 2, [4, 4], ["walk", "idle"])
-
         super().__init__(position, animation, (20, 15))
+        while True:
+            print("hello world")
+            
 
 
     def update(self):
