880e777502c91530a8b645f3f75193f138b87978
AaronCode2 <AaronWorkPC@outlook.com>
2026-09-05 18:00:32 +0100

    fixed stuff again

--- src/classes/ui/crafter.py
+++ src/classes/ui/crafter.py
@@ -8,7 +8,7 @@ class Crafter():
     def __init__(self):
 
         self.isCraftable = True
-        self.selectedRecipe = None)
+        self.selectedRecipe = None
 
     def update(self, window, inventory):
 
