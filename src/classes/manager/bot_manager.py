eb0297eb383871cd2cbca02a0f3df66f09a8444c
AaronCode2 <AaronWorkPC@outlook.com>
2026-09-14 05:46:26 +0100

    added bot spawning

--- src/classes/manager/bot_manager.py
+++ src/classes/manager/bot_manager.py
@@ -44,9 +44,17 @@ class BotManager():
             amount = 30 * self.day
 
             self.spawnBot(amount)
+            self.isBotToSpawn = False
+
+
 
         if(int(time.time()) - self.randomTimerSpawner > 10):
 
+            chance = random.randint(1, 100)
+
+            if(chance > 25): # 25%
+                self.spawnBot(random.randint(1, 10))
+            
             self.randomTimerSpawner = int(time.time()) 
         
 
